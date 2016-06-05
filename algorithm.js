439ec3d5d91cc90bfa88c345fba8503424867b9f
mckonieczny <mc.konieczny@gmail.com>
2016-06-05 18:33:40 +0200

    zmiana wyświetlania, refactor

--- algorithm.js
+++ algorithm.js
@@ -1,7 +1,6 @@
 var number_of_elements;
 var distancematrix_1;
 var XCollectionTable;
-var XCollectionTable2;
 var uOptimal;
 
 var initialVectorOfVisitedCities = new Array(1);
@@ -12,9 +11,9 @@ var initialVisitedCities = 0 ;
 var initialCost = 0 ;
 var initialX = 
 {
-	vectorOfCities: initialVectorOfVisitedCities ,
-	visitedCities: initialVisitedCities ,
-	vectorOfTime: initialVectorOfTime ,
+	vectorOfCities: initialVectorOfVisitedCities,
+	visitedCities: initialVisitedCities,
+	vectorOfTime: initialVectorOfTime,
 	cost: initialCost,
 	ifOnTime: true
 }
@@ -23,40 +22,29 @@ initialXCollection [0] = initialX;
 
 function calculate_init()
 {
-try {
-    console.clear();
-}
-catch(err) {
+	try {
+		console.clear();
+	}
+	catch(err) {}
 
-}
 	XCollectionTable = new Array ( number_of_elements + 1) ;
-	XCollectionTable2 = new Array ( number_of_elements + 1) ;
 	generateInitialCollection(initialXCollection,0);
-	XCollectionTable[0] = initialXCollection;
-	calculateXCollectionsNew(XCollectionTable2[number_of_elements],number_of_elements);
-	uOptimal = CalculateWayNewNew([0],0);
-	drawPathNew([]);
-	//showAllXCollections();
+	calculateXCollections(XCollectionTable[number_of_elements],number_of_elements);
+	uOptimal = calculateWay([0],0);
+	drawPath();
 }
 function ifOnTime(destinationCity,timeCome)
 {
-	//console.log(destinationCity+' '+timeCome);
 	if((locations[destinationCity].timeFrame.begin !=0 )&&(locations[destinationCity].timeFrame.begin>timeCome))
 	{
-		//console.log('City '+destinationCity+' false');
-		return 0;
+		return false;
 	}
 	if((locations[destinationCity].timeFrame.end!=0)&&(locations[destinationCity].timeFrame.end<timeCome))
 	{
-		//console.log('City '+destinationCity+' false');
-		return 0;
+		return false;
 	}
-	//console.log('City '+destinationCity+' true');
-	return 1;
+	return true;
 }
-//function calculateXCollections(,step)
-
-
 
 function addElementToCollection(givenCollection,givenElement)
 {
@@ -74,12 +62,12 @@ function addElementToCollection(givenCollection,givenElement)
 					break;
 				}
 			}
-			if (check == true)
+			if (check)
 			{
 				existsInCollection = true;
 				var tempHandler;
 				var nullHandler;
-				if( givenElement.cost <= givenCollection[i].cost)
+				if( givenElement.ifOnTime && givenElement.cost < givenCollection[i].cost)
 				{
 					tempHandler = givenCollection[i];
 					givenCollection[i] = givenElement;
@@ -134,51 +122,43 @@ function showXCollection ( givenCollection )
 }
 function showX (givenX)
 {
-	var debugInfo='State: ';
+	var debugInfo='state: ';
 	for (var i = 0; i <= givenX.vectorOfCities.length - 1; i++)
 	{
 		if(i!=0){debugInfo+=' -> ';}
 		debugInfo+=givenX.vectorOfCities[i];
 	}
-	var dateString = new Date( givenX.vectorOfTime[givenX.vectorOfTime.length-1] );
-	//debugInfo+=' time: '+ dateString +' cost: '+givenX.cost;
-	debugInfo+=' cost: '+givenX.cost;
-	//debugInfo+=' to visit: ';
-	//var notVisitedCities = Math.pow(2,number_of_elements) - 2 - givenX.visitedCities;
-	//for(var i=1 ; i <= number_of_elements ; i++ )
-	//{
-	//	if( Math.floor( notVisitedCities/Math.pow(2,i)) % 2 == 1)
-	//	{
-	//		debugInfo += i+' ';
-	//	}
-	//}
-	debugInfo += ' decision: '+givenX.decision;
+
+	var decisions = [];
+	var costs = [];
+	
+	decisions.push(givenX.decision);
+	costs.push(givenX.cost);
+	
 	if(givenX.rejected.length>0)
 	{
-			debugInfo+=' rejected cities: '
 		for( var i = 0 ; i < givenX.rejected.length ; i++ )
 		{
-			if(i!=0)
-			{
-				debugInfo+=', ';
+			if(!givenX.rejected[i].ifOnTime) {
+				console.log('Decision '+givenX.rejected[i].decision+' is out of time frame!');
 			}
-			debugInfo+=givenX.rejected[i].decision;// +' cost: '+givenX.rejected[i].cost+ ' ';
+			
+			decisions.push(givenX.rejected[i].decision);
+			costs.push(givenX.rejected[i].cost);
 		}	
 	}
 
+	debugInfo+=', decisions: ' + decisions + ', costs: ' + costs + ', best: ' + givenX.decision+'(' + givenX.cost + ')'
+	
 	console.log(debugInfo);
 }
-
-//function removeElementFromCollection()
-//{
-//}
 
 function showAllXCollections()
 {
 	for ( var i = 0 ; i < number_of_elements ; i++ )
 	{
 	console.log(' ');
-	showXCollection(XCollectionTable2[i]);
+	showXCollection(XCollectionTable[i]);
 	}
 }
 function resultX ( givenCollection )
@@ -205,15 +185,13 @@ function resultX ( givenCollection )
 	return resultXX;
 }
 
-
-
-function calculateXCollectionsNew(xCollection,step)
+function calculateXCollections(xCollection,step)
 {
 	if(step >0 )
 	{
-		var debugInfo = 'V'+(number_of_elements-step+1)+'(x'+(step-1)+')';
+		console.log('V'+(number_of_elements-step+1)+'(x'+(step-1)+')');
 	}
-	console.log(debugInfo);
+
 	var xppCollection = [];
 	if( step >= 1)
 	{
@@ -221,22 +199,12 @@ function calculateXCollectionsNew(xCollection,step)
 		{
 			xppCollection = possibilitiesForSingleXNew(xppCollection,xCollection[i]);
 		}
-		XCollectionTable2[step-1] = xppCollection;
-		showXCollection (XCollectionTable2[step-1]);
-		calculateXCollectionsNew(xppCollection,step-1);
+		XCollectionTable[step-1] = xppCollection;
+		showXCollection (XCollectionTable[step-1]);
+		calculateXCollections(xppCollection,step-1);
 	}
 	
 }
-
-
-
-
-
-
-
-
-
-
 
 function generateInitialCollection(xCollection,step)
 {
@@ -253,9 +221,9 @@ function generateInitialCollection(xCollection,step)
 	{
 		for( var i = 0 ; i < xCollection.length ; i++ )
 			{
-				xppCollection = addLastPath2( xppCollection,xCollection[i] );
+				xppCollection = addLastPath( xppCollection,xCollection[i] );
 			}
-		XCollectionTable2[step+1] = xppCollection;
+		XCollectionTable[step+1] = xppCollection;
 	}
 }
 function possibilitiesForSingleXInit(generatedXppCollection,X)
@@ -270,8 +238,6 @@ function possibilitiesForSingleXInit(generatedXppCollection,X)
 	{
 		if( Math.floor( notVisitedCities/Math.pow(2,i)) % 2 == 1)
 		{
-			if ( true )
-			{
 				var newVectorOfCities = new Array ( X.vectorOfCities.length + 1 );
 				for (var j = 0 ; j < X.vectorOfCities.length ; j++)
 				{
@@ -284,32 +250,21 @@ function possibilitiesForSingleXInit(generatedXppCollection,X)
 					newVectorOfTime[j] = X.vectorOfTime[j];
 				}
 				newVectorOfTime[newVectorOfTime.length-1] = X.vectorOfTime[X.vectorOfTime.length-1] + distancematrix_1[i][X.vectorOfCities[X.vectorOfCities.length-1]].duration*1000 ;
-				var newCost = 0;//X.cost + distancematrix_1[i][X.vectorOfCities[X.vectorOfCities.length-1]].distance ;
 				var newVisitedCities = X.visitedCities + Math.pow(2,i) ;
-				var rejected=[];
 				var newX = 
 				{
 					vectorOfCities: newVectorOfCities ,
 					visitedCities: newVisitedCities ,
 					vectorOfTime: newVectorOfTime ,
-					cost: newCost,
+					cost: 0,
 					ifOnTime: true,
-					rejected: rejected
+					rejected: []
 				};
 				generatedXppCollection = addElementToCollection (generatedXppCollection , newX);
-			}
-			else
-			{
-				//console.log('Not on time');
-			}
 		}
 	}
 	return generatedXppCollection;
 }
-
-
-
-
 
 function possibilitiesForSingleXNew(generatedXppCollection,X)
 {
@@ -320,8 +275,6 @@ function possibilitiesForSingleXNew(generatedXppCollection,X)
 	var minimumCostCityIndex=0;
 	var minimumCost = Number.MAX_VALUE;
 	
-			if ( ifOnTime(X.vectorOfCities[X.vectorOfTime.length-2], X.vectorOfTime[X.vectorOfTime.length-2]))
-			{
 				var newVectorOfCities = new Array ( X.vectorOfCities.length - 1 );
 				for (var j = 0 ; j < X.vectorOfCities.length -1 ; j++)
 				{
@@ -333,8 +286,7 @@ function possibilitiesForSingleXNew(generatedXppCollection,X)
 					newVectorOfTime[j] = X.vectorOfTime[j];
 				}
 				var newCost = X.cost + distancematrix_1[X.vectorOfCities[X.vectorOfCities.length-2]][X.vectorOfCities[X.vectorOfCities.length-1]].distance ;
-				var newVisitedCities = X.visitedCities;// + Math.pow(2,i) ;
-				var rejected=[];
+				var newVisitedCities = X.visitedCities;
 				var newX = 
 				{
 					vectorOfCities: newVectorOfCities ,
@@ -342,31 +294,17 @@ function possibilitiesForSingleXNew(generatedXppCollection,X)
 					vectorOfTime: newVectorOfTime ,
 					cost: newCost,
 					decision:X.vectorOfCities[X.vectorOfTime.length-1],
-					ifOnTime: true,
-					rejected:rejected
+					ifOnTime: ifOnTime(X.vectorOfCities[X.vectorOfTime.length-2], X.vectorOfTime[X.vectorOfTime.length-2]),
+					rejected:[]
 				};
 				generatedXppCollection = addElementToCollection (generatedXppCollection , newX);
-			}
-			else
-			{
-				/*var debugInfo='Not on time: State: ';
-				for (var i = 0; i <= X.vectorOfCities.length - 1; i++)
-				{
-					if(i!=0){debugInfo+=' -> ';}
-					debugInfo+=X.vectorOfCities[i];
-				}
-				debugInfo+=' decision: '+X.decision;
-				console.log(debugInfo);*/
-				;
-			}
 
 	return generatedXppCollection;
 }
 
-function addLastPath2(generatedXppCollection,X)
+function addLastPath(generatedXppCollection,X)
 {
-	if ( ifOnTime(0, X.vectorOfTime[X.vectorOfTime.length-1] + distancematrix_1[0][X.vectorOfCities[X.vectorOfCities.length-1]].duration*1000 ) )
-	{
+
 		var newVectorOfCities = new Array ( X.vectorOfCities.length + 1 );
 		for (var j = 0 ; j < X.vectorOfCities.length ; j++)
 		{
@@ -379,37 +317,30 @@ function addLastPath2(generatedXppCollection,X)
 			newVectorOfTime[j] = X.vectorOfTime[j];
 		}
 		newVectorOfTime[newVectorOfTime.length-1] = X.vectorOfTime[X.vectorOfTime.length-1] + distancematrix_1[0][X.vectorOfCities[X.vectorOfCities.length-1]].duration*1000 ;
-		var newCost = 0;//X.cost + distancematrix_1[0][X.vectorOfCities[X.vectorOfCities.length-1]].distance ;
 		var newVisitedCities = X.visitedCities ;
-		var decision;
 		var newX = 
 		{
 			vectorOfCities: newVectorOfCities ,
 			visitedCities: newVisitedCities ,
 			vectorOfTime: newVectorOfTime ,
-			cost: newCost,
-			decision:decision,
-			ifOnTime: true
+			cost: 0,
+			decision: undefined,
+			ifOnTime: ifOnTime(0, X.vectorOfTime[X.vectorOfTime.length-1] + distancematrix_1[0][X.vectorOfCities[X.vectorOfCities.length-1]].duration*1000 )
 		};
 				generatedXppCollection = addElementToCollection (generatedXppCollection , newX);
-			}
-			else
-			{
-//				console.log('Not on time');
-			}
+
 	return generatedXppCollection;
 }
-function psi(X,step)
+function psi(U,step)
 {
 	var minCost = Number.MAX_VALUE;
 	var minIndeks = -1 ;
-	for( var i = 0 ; i < XCollectionTable2[step].length ; i++ )
+	for( var i = 0 ; i < XCollectionTable[step].length ; i++ )
 	{
 		var check = true ;
 		for( var j = 0 ; j < step + 1;j++)
 		{
-			//console.log('i = '+i+' j= '+'if left= '+XCollectionTable2[step+1][i].vectorOfCities[j]+' if right '+X.vectorOfCities[j]);
-			if(XCollectionTable2[step][i].vectorOfCities[j]!=X.vectorOfCities[j])
+			if(XCollectionTable[step][i].vectorOfCities[j]!=U[j])
 			{
 				check = false;
 				break;
@@ -417,62 +348,23 @@ function psi(X,step)
 		}
 		if (check == true)
 		{
-			if( minCost > XCollectionTable2[step][i].cost)
+			if( minCost > XCollectionTable[step][i].cost)
 			{
-				minCost = XCollectionTable2[step][i].cost;
+				minCost = XCollectionTable[step][i].cost;
 				minIndeks = i;
 			}
 		}
 	}
 	if(minCost < Number.MAX_VALUE)
 	{
-		var result = {
-			decision: XCollectionTable2[step][minIndeks].decision,
-			//decision: minIndeks;
-			x:XCollectionTable2[step][minIndeks]
-		}
-		return result;
+		return XCollectionTable[step][minIndeks].decision;
 	}
 	else
 	{
-		return minIndeks;
+		return undefined;
 	}
 }
-function psi2(U,step)
-{
-	var minCost = Number.MAX_VALUE;
-	var minIndeks = -1 ;
-	for( var i = 0 ; i < XCollectionTable2[step].length ; i++ )
-	{
-		var check = true ;
-		for( var j = 0 ; j < step + 1;j++)
-		{
-			if(XCollectionTable2[step][i].vectorOfCities[j]!=U[j])
-			{
-				check = false;
-				break;
-			}
-		}
-		if (check == true)
-		{
-			if( minCost > XCollectionTable2[step][i].cost)
-			{
-				minCost = XCollectionTable2[step][i].cost;
-				minIndeks = i;
-			}
-		}
-	}
-	if(minCost < Number.MAX_VALUE)
-	{
-		return XCollectionTable2[step][minIndeks].decision;
-	}
-	else
-	{
-		var temp;
-		return temp;
-	}
-}
-function CalculateWayNewNew(uVector,step)
+function calculateWay(uVector,step)
 {
 	if(step < number_of_elements )
 	{
@@ -481,9 +373,8 @@ function CalculateWayNewNew(uVector,step)
 		{
 			uVectorNew[j]=uVector[j];
 		}
-		uVectorNew[uVectorNew.length-1] = psi2(uVector,step);
-		ShowUVector(uVectorNew);
-		return CalculateWayNewNew(uVectorNew,step+1);
+		uVectorNew[uVectorNew.length-1] = psi(uVector,step);
+		return calculateWay(uVectorNew,step+1);
 	}
 	else
 	{
@@ -495,11 +386,14 @@ function CalculateWayNewNew(uVector,step)
 		return uVectorNew;
 	}
 }
-function drawPathNew(route)
+function drawPath()
 {
-	//if((typeof uOptimal[0] !== undefined )&&(uOptimal[0]!="undefined")&&(typeof uOptimal[0] >=0 ))
+	var route = [];
 	if(!isNaN(uOptimal[0]))
 	{
+		var now = new Date();
+		console.log('Choose path: ');
+		
 		var patchVector = new Array ( uOptimal.length + 1 );
 		for (var j = 0 ; j < uOptimal.length ; j++)
 		{
@@ -513,34 +407,29 @@ function drawPathNew(route)
 			{
 				timeCurrent += distancematrix_1[patchVector[i]][patchVector[i-1]].duration*1000;
 			}
+			
+			if (i==0) {
+				console.log('state: ' + locations[patchVector[i]].name + '(' + locations[patchVector[i]].id + '), starting: ' + formatDate(now));
+			} else {
+				console.log('state: ' + route.map(function(id) {return locations[id].id;}).join(' -> ') + ', desision: ' + locations[patchVector[i]].name + '(' + locations[patchVector[i]].id + '), incoming: '+formatDate(new Date( timeCurrent )));
+			}
+
 			route.push(patchVector[i]);
-			var dateString = new Date( timeCurrent );
-			console.log('City: '+patchVector[i]+' '+locations[patchVector[i]].name+' @ '+ dateString); 
 		}
 		route = route.map(function(id) {
 			return locations[id];
 		});
+		console.log('path: ' + route.map(function(loc) {return loc.id;}).join(' -> ') + ', cost: ' + XCollectionTable[0][0].cost);
+
 		drawRoute(route);
 	}
 	else
 	{
-		console.log('It is not possible.');
-		alert('It is not possible.');
+		console.log('Nie znaleziono drogi spełniającej oczekiwania.');
+		alert('Nie znaleziono drogi spełniającej oczekiwania.');
 	}
 }
-function ShowUVector(uVector)
-{
-	if(!isNaN(uVector[0]))
-	{
-		var toPrint = 'u = ';
-		for (var j = 1 ; j < uVector.length ; j++)
-			{
-				if(j!=1)
-				{
-					toPrint+= ' -> ';
-				}
-				toPrint+= uVector[j];
-			}
-		console.log(toPrint);
-	}
+
+function formatDate(date) {
+	return date.getDate() + '/' + (date.getMonth()+1) + '/' +date.getFullYear()  + ' ' + date.getHours() + ":" + (date.getMinutes()<10?'0':'') + date.getMinutes()
 }
